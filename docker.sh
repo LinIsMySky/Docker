68fba4f806fafdbf933a50f593d7ba725b2c2962
Yin <1240596473@qq.com>
2019-04-25 22:35:45 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -18,7 +18,5 @@ apt-get install docker-ce
 cd ~
 mkdir portainer
 cd ./portainer
-docker volume create portainer_data
-docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
 docker stack deploy --compose-file=portainer-agent-stack.yml portainer
